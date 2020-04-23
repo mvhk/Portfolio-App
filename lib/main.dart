6de3cb92cf13c6a8b85296cf4885e6d5111f1f1e
Venkata Harish Kumar Mamidi <harish.mamidi.kumar@gmail.com>
2020-04-23 11:44:12 +0530

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,71 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+        backgroundColor: Colors.white12,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage("images/harish.png"),
+              ),
+              Text(
+                "Harish Mamidi",
+                style: TextStyle(
+                  fontFamily: "Pacifico",
+                  fontSize: 30.0,
+                  color: Colors.white,
+                ),
+              ),
+              Text(
+                "FLUTTER DEVELOPER",
+                style: TextStyle(
+                    color: Colors.white, letterSpacing: 2.5, fontSize: 13.0),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 130.0,
+                child: Divider(color: Colors.white60),
+              ),
+              Card(
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.lightBlue,
+                    ),
+                    title: Text(
+                      "9494660109",
+                      style: TextStyle(color: Colors.blue, fontSize: 15),
+                    ),
+                  )),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.blue,
+                  ),
+                  title: Text(
+                    "harish.mamidi.kumar@gmail.com",
+                    style: TextStyle(color: Colors.blue),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+      debugShowCheckedModeBanner: false,
+    );
+  }
 }
